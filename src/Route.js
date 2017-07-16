933160ff00b2810237e780fecaf6c61505101e54
Deepjyoti <deepjyoti@hike.in>
2017-07-16 19:09:33 +0530

    adding navigation and disable back button for some employee list scene

--- src/Route.js
+++ src/Route.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import LoginForm from './components/LoginForm';
 import {Scene, Router} from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 class Route extends Component {
   render() {
     return (
       <Router>
         <Scene cardStyle={ {backgroundColor: '#fff' } } key="root">
-          <Scene key="LoginForm" component={LoginForm} title="Please Login"/>
+          <Scene key="loginForm" component={LoginForm} title="Please Login" />
+          <Scene
+            onRight={() => console.log('right')}
+            rightTitle="Add"
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+            renderLeftButton={true}/>
         </Scene>
       </Router>
     );
