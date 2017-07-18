064e1a5239fb0823851b46c6a15d5410358a6a94
Deepjyoti <deepjyoti@hike.in>
2017-07-19 01:26:16 +0530

    delete the employee from firebase

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
@@ -29,7 +29,9 @@ class EmployeeEdit extends Component {
   }
 
   onAccept() {
-
+    const { uid } = this.props.employee;
+    this.props.employeeDelete({ uid });
+    this.setState({ showModal: false });
   }
 
   onDecline() {
@@ -78,5 +80,6 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps, {
   employeeUpdate,
-  employeeSave
+  employeeSave,
+  employeeDelete
 })(EmployeeEdit);
